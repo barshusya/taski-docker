f7bdc28e2f7932375220a1c7c25099a5dd7d213d
Anastasia Barshina <barshina.av@gmail.com>
2024-04-20 14:47:59 +0300

    Update backend settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY_TASKI', '')
+SECRET_KEY = os.getenv('SECRET_KEY_TASKI', 'lajdvlkj')
 
 DEBUG = False
 
